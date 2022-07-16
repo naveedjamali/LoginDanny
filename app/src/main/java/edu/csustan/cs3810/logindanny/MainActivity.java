73d0c934037d74dfaf05ff45f18b3b298c82e533
Naveed Jamali <naveedjamali@outlook.com>
2022-07-16 13:17:55 +0500

    Initial commit

---
+++ app/src/main/java/edu/csustan/cs3810/logindanny/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.csustan.cs3810.logindanny;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
